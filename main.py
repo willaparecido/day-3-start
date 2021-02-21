ccb2c162b745dd510f95b9e032f9a31a9601f9e0
willaparecido <wilson_aparecido2015@outlook.com>
2021-02-21 10:46:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+  print ("you can ride the rollercoster!")
+else:
+  print("sorry, you have to grow taller before you can ride.")
+
+
+
+
+  #if - Conditional >>> SE
+  #else - Conditional >>> Se não
+
+  #operators 
+  # >  Greater than 
+  # <  less than 
+  # >=  Greater than or equal to
+  # <=  less thanor equal to
